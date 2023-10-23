785453e7796e2f7589d87e0323c2b0ca6a03ef6b
Unicorn <114812731+sahilgoyal7214@users.noreply.github.com>
2023-10-23 23:10:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,13 +42,13 @@ rfr_model = load_rfr_model(model_path)
 
 if rfr_model is not None:
     store = st.number_input("Enter store number: ",value = 0)
-    Fuel_Price = st.number_input("Enter fuel price: ")
+    Fuel_Price = st.number_input("Enter fuel price: ",)
     CPI = st.number_input("Enter CPI: ")
     Unemployment = st.number_input("Enter unemployment: ")
-    Day = st.number_input("Enter day: ")
-    Month = st.number_input("Enter month: ")
-    Year = st.number_input("Enter year: ")
-    holiday_flag = st.number_input("Is it a holiday?(0 for no, 1 for yes): ")
+    Day = st.number_input("Enter day: ",value = 0)
+    Month = st.number_input("Enter month: ",value = 0)
+    Year = st.number_input("Enter year: ",value = 0)
+    holiday_flag = st.number_input("Is it a holiday?(0 for no, 1 for yes): ",min_value = 0,max_value = 1)
 
     if st.button("Predict"):
         input_data = array([[store, Fuel_Price, CPI, Unemployment, Day, Month, Year, holiday_flag]])
